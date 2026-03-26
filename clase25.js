831083568c9b884c991338b7f6a72e01270c7637
Miguel Garcia <agcmiguel15@gmail.com>
2026-03-25 18:08:30 -0600

    s

---
+++ clase25.js
@@ -0,0 +1,8 @@
+/*apuntar a un elemento que tiene un id llamado titulo */
+//del documento necesito obtener un elemento que tiene id "titulo"
+
+    //document.getElementById("titulo")
+/*obtener el texto que tiene ese elemento
+ para eso usamos innerText
+*/
+document.getElementById("titulo").innerText
